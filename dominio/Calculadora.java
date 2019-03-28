12e2ae26242d6ba904a2abbb902fee197698557c
MiYazJE <ruben.saizserrano@gmail.com>
2019-03-28 05:21:13 +0100

    Cosas...

--- dominio/Calculadora.java
+++ dominio/Calculadora.java
@@ -7,15 +7,16 @@ public class Calculadora {
 	// ATRIBUTOS
 	private double num1;
 	private double num2;
+	
 	private String operacion;
 	private String numActual;
 	private String numMemoria;
 	
+	boolean mostrar;
+	
 	// CONSTRUCTOR
 	public Calculadora() {
-		this.num1 = this.num2 = 0;
-		this.operacion = this.numMemoria = "";
-		this.numActual = "0";
+		reset();
 	}
 
 	// GETTERS AND SETTERS
@@ -23,10 +24,18 @@ public class Calculadora {
 		return num1;
 	}
 
-	public void setNum1(double num1) {
-		this.num1 = num1;
+	public void setNum1(double num) {
+		this.num1 = num;
+	}
+	
+	public boolean getMostrar() {
+		return mostrar;
 	}
 
+	public void setMostrar(boolean mostrar) {
+		this.mostrar = mostrar;
+	}
+	
 	public double getNum2() {
 		return num2;
 	}
@@ -99,16 +108,16 @@ public class Calculadora {
 		num1 = num2 = 0;
 		operacion = numMemoria = "";
 		numActual = "0";
+		mostrar = false;
 	}
 	
 	// CONCATENAR NUMERO AL NUMERO ACTUAL
-	public void concatenar(String num) { this.numActual += num; }
+	public void concatenar(String num) { 
+		this.numActual += num; 
+	}
 	
 	public void retroceder() {
-		if (numActual.length() == 1) numActual = "0";
-		else if (numActual.length() > 1) {
-			numActual = numActual.substring(0, numActual.length()-1);
-		}
+		numActual = numActual.substring(0, numActual.length()-1);
 	}
 	
 	public void sumarMemoria() {
@@ -124,30 +133,20 @@ public class Calculadora {
 	public double calcular() throws DivisionPorCeroExcepcion {
 		
 		switch (this.operacion) {
-		
 		case "+": return sumar();
 		case "*": return multiplicar();
 		case "/": return dividir();
 		case "-": return restar();
-		
 		}
 		
 		return 0;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "num1 = " + num1 + "\nnum2 = " + num2 + "\noperacion = " + operacion + "\nnumActual = " + numActual
+				+ "\nnumMemoria = " + numMemoria;
+	}
 	
 	
 	
