dd529d83afec2e8c6601d25b568b995862ba58f2
Rubén <42714332+MiYazJE@users.noreply.github.com>
2019-04-02 02:34:21 +0200

    Gestión de errores

---
+++ interfaces/Errores.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Errores {
+
+	public final String ERROR_DIVISION_POR_0       = "No se puede dividir por 0";
+	public final String ERROR_RAIZ_NUMERO_NEGATIVO = "Entrada no valida";
+	
+}
